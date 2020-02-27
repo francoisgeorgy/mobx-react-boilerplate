9417f901362686b57ba172e13445558ac9b8d6dd
François Georgy <francois.georgy@gmail.com>
2020-02-27 17:20:09 +0100

    code formating

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-dom";
+import {render} from "react-dom";
 import DevTools from "mobx-react-devtools";
 
 import TodoList from "./components/TodoList";
@@ -9,11 +9,11 @@ import TodoModel from "./models/TodoModel";
 const store = new TodoListModel();
 
 render(
-  <div>
-    <DevTools />
-    <TodoList store={store} />
-  </div>,
-  document.getElementById("root")
+    <div>
+        <DevTools/>
+        <TodoList store={store}/>
+    </div>,
+    document.getElementById("root")
 );
 
 store.addTodo("Get Coffee");
@@ -21,7 +21,7 @@ store.addTodo("Write simpler code");
 store.todos[0].finished = true;
 
 setTimeout(() => {
-  store.addTodo("Get a cookie as well");
+    store.addTodo("Get a cookie as well");
 }, 2000);
 
 // playing around in the console
